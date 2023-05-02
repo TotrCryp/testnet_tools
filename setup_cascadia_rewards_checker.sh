9b923b193043818282ee517754c63d86ab085b9a
Totr <110344016+TotrCryp@users.noreply.github.com>
2023-05-02 14:23:57 +0300

    Update setup_cascadia_rewards_checker.sh

--- setup_cascadia_rewards_checker.sh
+++ setup_cascadia_rewards_checker.sh
@@ -18,7 +18,7 @@ read -r -p "Continue? [y/n]" response
             wget -q https://raw.githubusercontent.com/TotrCryp/testnet_tools/main/cascadia_rewards_checker.sh && \
             read -p "Enter address (looks like cascadia123...): " address && \
             read -p "Enter validator (looks like cascadiavaloper123...): " validator && \
-            read -p "Enter password (should look like something incomprehensible): " password && \
+            read -p "Enter keyring passphrase (should look like something incomprehensible): " password && \
             sed -i 's/^address="".*/address=$address/; s/^validator="".*/validator=$validator/; s/^password="".*/password=$password/' cascadia_rewards_checker.sh && \
             chmod +x cascadia_rewards_checker.sh
             crontab -l | { cat; echo "*/30 * * * * $HOME/cascadia_rewards_checker.sh"; } | crontab -
