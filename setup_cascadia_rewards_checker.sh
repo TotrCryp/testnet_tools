c2b088ced617a8b332f6b333db972269dc9452cb
Totr <110344016+TotrCryp@users.noreply.github.com>
2023-05-02 12:34:34 +0300

    Update setup_cascadia_rewards_checker.sh

--- setup_cascadia_rewards_checker.sh
+++ setup_cascadia_rewards_checker.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 totr_logo_link="https://raw.githubusercontent.com/TotrCryp/assets/main/totr_logo.sh"
-
 wget -q --spider $totr_logo_link
 . <(wget -qO- $totr_logo_link)
 echo -e "Setup Cascadia rewards checker..."
 cd
-wget https://raw.githubusercontent.com/TotrCryp/testnet_tools/main/cascadia_rewards_checker.sh && \
+wget -q --spider https://raw.githubusercontent.com/TotrCryp/testnet_tools/main/cascadia_rewards_checker.sh && \
 read -p "Enter address: " address && \
 read -p "Enter validator: " validator && \
 read -p "Enter password: " password && \
