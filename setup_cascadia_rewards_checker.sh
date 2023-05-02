7ed4b12be8242231aaf53fb4472cf2622b074092
Totr <110344016+TotrCryp@users.noreply.github.com>
2023-05-02 13:34:56 +0300

    Update setup_cascadia_rewards_checker.sh

--- setup_cascadia_rewards_checker.sh
+++ setup_cascadia_rewards_checker.sh
@@ -4,10 +4,28 @@ totr_logo_link="https://raw.githubusercontent.com/TotrCryp/assets/main/totr_logo
 wget -q --spider $totr_logo_link
 . <(wget -qO- $totr_logo_link)
 echo -e "Setup Cascadia rewards checker..."
+echo -e "The script checks for claimable  rewards every 30 minutes."
+echo -e "If the amount of rewards exceeds 10000000000000000 aCC, rewards are received (including commissions)."
+echo -e "Then the available balance of the address is checked and the available amount for the restake is determined"
+echo -e "(as available_balance - 10000000000000000 so as not to empty the wallet),"
+echo -e "and delegation to the validator takes."
+echo -e "Logs are stored in /var/log/cascadia_rewards_checker.log"
+confirm() {
+    read -r -p "${1:-Continue? [y/n]} " response
+    case "$response" in
+        [yY][eE][sS]|[yY])
+            true
+            ;;
+        *)
+            false
+            ;;
+    esac
+}
 cd $HOME
 wget -q https://raw.githubusercontent.com/TotrCryp/testnet_tools/main/cascadia_rewards_checker.sh && \
-read -p "Enter address: " address && \
-read -p "Enter validator: " validator && \
-read -p "Enter password: " password && \
+read -p "Enter address (looks like cascadia123...): " address && \
+read -p "Enter validator (looks like cascadiavaloper123...): " validator && \
+read -p "Enter password (should look like something incomprehensible): " password && \
 sed -i 's/^address="".*/address="$address"/; s/^validator="".*/validator="$validator"/; s/^password="".*/password="$password"/' cascadia_rewards_checker.sh && \
 chmod +x cascadia_rewards_checker.sh
+echo "*/30 * * * * /$HOME/cascadia_rewards_checker.sh" | crontab -
