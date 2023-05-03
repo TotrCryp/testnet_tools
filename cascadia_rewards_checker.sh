37f0d9fc245a8e1713dde21712897f8abca5e7b0
Totr <110344016+TotrCryp@users.noreply.github.com>
2023-05-03 11:37:00 +0300

    Update cascadia_rewards_checker.sh

--- cascadia_rewards_checker.sh
+++ cascadia_rewards_checker.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Set the PATH variable
-PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin
+PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/usr/local/go/bin:/root/go/bin
 
 # Set the address and validator information
 address=""
 validator=""
 password=""
-min_reward="10000000000000000"  # Minimum reward amount to withdraw and restake in udenom
+min_reward="500000000000000"  # Minimum reward amount to withdraw and restake in udenom
 log_file="/var/log/cascadia_rewards_checker.log"
 
 # Get the available rewards
@@ -19,17 +19,19 @@ if [[ $rewards -ge $min_reward ]]; then
         # Withdraw and restake the rewards
         echo "$(date): Withdrawing and restaking rewards..." | tee -a $log_file
         echo "$(date): Withdrawing:" | tee -a $log_file
-        cascadiad tx distribution withdraw-rewards $validator --from $address --gas-adjustment=1.2 --gas-prices=10aCC --commission -y <<< "$password" >> $log_file
+        cascadiad tx distribution withdraw-rewards $validator --from $address --gas auto --gas-adjustment=1.3 --gas-prices=10aCC --commission -y <<< "$password" >> $log_file
         sleep 5
-	p_balance=$(cascadiad q bank balances $address)
-	a_balance=$(echo "$p_balance" | sed -n '/balances:/,/^$/p' | sed -n '2p' | sed 's/- amount: "//;s/"$//')
-	balance=$(echo $a_balance | cut -d'.' -f1)
-	echo "$(date): Available balance: $balance" | tee -a $log_file
-	RestakAmount = $($balance - $min_reward)
-	if [[ $RestakAmount -ge $min_reward ]]; then
-		echo "$(date): Restaking:" | tee -a $log_file
-		cascadiad tx staking delegate $validator "$RestakAmount"aCC --from $address --gas-adjustment=1.2 --gas-prices=10aCC -y <<< "$password" >> $log_file
-	fi
+        p_balance=$(cascadiad q bank balances $address)
+        a_balance=$(echo "$p_balance" | sed -n '/balances:/,/^$/p' | sed -n '2p' | sed 's/- amount: "//;s/"$//')
+        balance=$(echo $a_balance | cut -d'.' -f1)
+        echo "$(date): Available balance: $balance" | tee -a $log_file
+        RestakAmount=$(($balance-$min_reward))
+        sleep 2
+        if [[ $RestakAmount -ge $min_reward ]]; then
+                echo "$(date): Restaking:" | tee -a $log_file
+                echo "Restake amoumt: $RestakAmount" | tee -a $log_file
+                cascadiad tx staking delegate $validator "$RestakAmount"aCC --from $address --gas auto --gas-adjustment=1.3 --gas-prices=10aCC -y <<< "$password" >> $log_file
+        fi
 else
         # Log that the reward amount is too low
         echo "$(date): Reward amount ($rewards udenom) is below the minimum threshold ($min_reward udenom)." | tee -a $log_file
